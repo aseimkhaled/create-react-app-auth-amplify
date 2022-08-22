a4b7ff88a3823528f15166e1240a2c07795dfbe9
Asim Alothaim <37918470+aseimkhaled@users.noreply.github.com>
2022-08-22 16:23:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
-
-class App extends Component {
+class HelloMessage extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hi asm
-          </a>
-        </header>
-      </div>
-    );
+    return <div>Hello {this.props.name}</div>;
   }
 }
 
-export default withAuthenticator(App);
+root.render(<HelloMessage name="Taylor" />);
